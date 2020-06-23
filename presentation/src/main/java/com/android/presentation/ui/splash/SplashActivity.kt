59fa79c33e87815d696f346b039d9b2a1067dee8
Hassan Alizadeh <hassan.alizadeh529@gmail.com>
2020-06-23 22:17:09 +0430

    Add a fake Activity to run project

---
+++ presentation/src/main/java/com/android/presentation/ui/splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.android.presentation.ui.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.presentation.R
+
+/**
+ * Created by hassanalizadeh on 23,June,2020
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
