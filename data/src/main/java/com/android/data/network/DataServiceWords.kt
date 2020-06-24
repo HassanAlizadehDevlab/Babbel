bb82828fad3672c901f9c1aeb304069442a867f6
Hassan Alizadeh <hassan.alizadeh529@gmail.com>
2020-06-25 01:18:56 +0430

    Change url of getWordList

--- data/src/main/java/com/android/data/network/DataServiceWords.kt
+++ data/src/main/java/com/android/data/network/DataServiceWords.kt
@@ -10,7 +10,7 @@ import retrofit2.http.GET
  */
 interface DataServiceWords {
 
-    @GET("/7ac6cdb4bf5e032f4c737aaafe659b33/raw/baa9fe0d586082d85db71f346e2b039c580c5804/words.json")
+    @GET("words.json")
     fun words(): Single<List<Word>>
 
 }
