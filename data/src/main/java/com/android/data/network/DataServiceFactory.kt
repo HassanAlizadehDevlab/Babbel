36d0f342f78c647dad1cdec87d9f71ed84bbcbaa
Hassan Alizadeh <hassan.alizadeh529@gmail.com>
2020-06-25 01:15:13 +0430

    Add Base_Url

--- data/src/main/java/com/android/data/network/DataServiceFactory.kt
+++ data/src/main/java/com/android/data/network/DataServiceFactory.kt
@@ -1,5 +1,6 @@
 package com.android.data.network
 
+import com.android.data.BuildConfig
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -17,7 +18,7 @@ class DataServiceFactory @Inject constructor() {
     fun <T> create(serviceClass: Class<T>): T = retrofit().create(serviceClass)
 
     private fun retrofit(): Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl(BuildConfig.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .client(okHttpClientBuilder().build())
@@ -28,9 +29,5 @@ class DataServiceFactory @Inject constructor() {
         .writeTimeout(15, TimeUnit.SECONDS)
         .readTimeout(15, TimeUnit.SECONDS)
         .retryOnConnectionFailure(true)
-
-    companion object {
-        private const val BASE_URL = "https://gist.githubusercontent.com/DroidCoder"
-    }
 
 }
