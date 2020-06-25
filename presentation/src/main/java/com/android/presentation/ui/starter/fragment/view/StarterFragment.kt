f28fd43dbcc923d2c0b2ca315098c3f45bd664e1
Hassan Alizadeh <hassan.alizadeh529@gmail.com>
2020-06-25 14:00:46 +0430

    Check passed word availability

--- presentation/src/main/java/com/android/presentation/ui/starter/fragment/view/StarterFragment.kt
+++ presentation/src/main/java/com/android/presentation/ui/starter/fragment/view/StarterFragment.kt
@@ -5,12 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.presentation.R
+import com.android.presentation.common.extension.*
 import com.android.presentation.common.view.BaseFragment
+import com.android.presentation.common.viewmodel.ViewModelProviderFactory
+import com.android.presentation.ui.starter.fragment.viewmodel.StarterViewModel
+import kotlinx.android.synthetic.main.fragment_starter.*
+import javax.inject.Inject
 
 /**
  * Created by hassanalizadeh on 25,June,2020
  */
 class StarterFragment : BaseFragment() {
+
+    @Inject
+    lateinit var factory: ViewModelProviderFactory
+    private lateinit var viewModel: StarterViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = viewModelProvider(factory)
+        observe(viewModel.passedWordAvailability, ::passedWordsAvailability)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +36,19 @@ class StarterFragment : BaseFragment() {
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
         super.onViewStateRestored(savedInstanceState)
+        buttonNext.disable()
+    }
+
+    private fun passedWordsAvailability(available: Boolean) {
+        progressBarLoading.gone()
+
+        buttonNext.text = if (available) {
+            getString(R.string.continue_)
+        } else {
+            getString(R.string.start)
+        }
+
+        buttonNext.enable()
     }
 
     companion object {
--- presentation/src/main/java/com/android/presentation/ui/starter/fragment/viewmodel/StarterViewModel.kt
+++ presentation/src/main/java/com/android/presentation/ui/starter/fragment/viewmodel/StarterViewModel.kt
@@ -1,5 +1,8 @@
 package com.android.presentation.ui.starter.fragment.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import com.android.domain.usecase.invoke
+import com.android.domain.usecase.word.PassedWordsAvailabilityUseCase
 import com.android.presentation.common.view.BaseViewModel
 import javax.inject.Inject
 
@@ -7,5 +10,19 @@ import javax.inject.Inject
  * Created by hassanalizadeh on 25,June,2020
  */
 class StarterViewModel @Inject constructor(
+    passedWordsAvailabilityUseCase: PassedWordsAvailabilityUseCase
+) : BaseViewModel() {
 
-): BaseViewModel()
+    val passedWordAvailability = MutableLiveData<Boolean>()
+
+    init {
+        passedWordsAvailabilityUseCase
+            .invoke()
+            .subscribe({
+                passedWordAvailability.value = true
+            }, {
+                passedWordAvailability.value = false
+            }).track()
+    }
+
+}
